9d7a1a00fbcb0e5645111257d014a256fedaa617
Niklas Henrich <HenrichN@users.noreply.github.com>
2018-01-17 21:31:47 +0100

    Render weeks with style

--- src/components/week.tsx
+++ src/components/week.tsx
@@ -3,6 +3,14 @@ import * as _ from "lodash";
 import { Moment } from "moment";
 
 export function renderWeek(week: Moment[]): VNode {
-  const days = _.map(week, day => <span>{day.format('D.M')}</span>);
-  return <div>{days}</div>;
+  const days = _.map(week, day =>
+      <div className='day'>
+        <div className='date-container'>
+          <span className='date'>
+            {day.format('D.M')}
+          </span>
+        </div>
+      </div>
+  );
+  return <div className='week'>{days}</div>;
 }
