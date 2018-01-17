a05cca0cf4c2826c006fae45d622b00d6d0e14c2
Niklas Henrich <HenrichN@users.noreply.github.com>
2018-01-17 21:17:29 +0100

    Fix date computation

--- src/components/week.tsx
+++ src/components/week.tsx
@@ -1,7 +1,8 @@
 import { VNode, span, div } from "@cycle/dom";
 import * as _ from "lodash";
+import { Moment } from "moment";
 
-export function renderWeek(week: any): VNode {
-  const days = _.map(week, day => <span>day</span>);
+export function renderWeek(week: Moment[]): VNode {
+  const days = _.map(week, day => <span>{day.format('D.M')}</span>);
   return <div>{days}</div>;
 }
--- src/date-helpers.ts
+++ src/date-helpers.ts
@@ -4,14 +4,13 @@ import * as moment from 'moment';
 export function getWeeks() {
   const WINTERPOKAL_START_DATE = '2017-10-30';
   const start = moment(WINTERPOKAL_START_DATE).startOf('isoweek' as any);
-  console.log(start);
+  // console.log(start.format());
   const end = moment().endOf('isoweek' as any);
-  console.log(end);
+  // console.log(end.format());
   var days = [];
   for (let m = moment(start); m.diff(end, 'days') <= 0; m.add(1, 'days')) {
-    days.push(m);
+    days.push(moment(m));
   }
   days.pop();
-  console.log(_.chunk(days, 7));
   return _.chunk(days, 7);
 }
